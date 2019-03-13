08e0b1b78aaff0afe6633e9c1737e3c1dc913641
Dominik Zawlocki <40212297+Ajver@users.noreply.github.com>
2019-03-13 17:16:27 +0100

    Sword, logo, footer

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 
-window.addEventListener('scroll', () => {
-    let pageLogo = document.querySelector('#page-logo');
-    let scrollTop = window.scrollY;
-    if(scrollTop > 0) {
-        pageLogo.classList.add('sticy');
-    }else {
-        pageLogo.classList.remove('sticy');
-    }
+const getElementHeight = elementName => {
+    return document.querySelector(elementName).clientHeight;
+}
+
+const getTopBarHeight = () => {
+    return getElementHeight('.top-bar');
+}
+
+const scrollToSection = (sectionName, time) => {
+    $.scrollTo($(sectionName).offset().top - getTopBarHeight(), time);
+}
+
+document.querySelector('#sword').addEventListener('click', () => {
+    scrollToSection('main', 500);
 });
