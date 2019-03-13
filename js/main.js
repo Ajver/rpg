69032a0acf7d407c561f9f55f88b1da281c9332d
Ajver <zawlockidominik@gmail.com>
2019-03-13 23:08:12 +0100

    Add gallery + systems pages

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 
 (function(){
-
+ 
   const initSystems = () => {
     const getElementHeight = elementName => {
         return document.querySelector(elementName).clientHeight;
@@ -21,23 +21,9 @@
     removeCurtain();
   }
 
-  let curtain = document.createElement('div');
-  curtain.classList.add('loading-curtain');
-  curtain.style = `
-    position: fixed;
-    width: 100vw;
-    height: 100vh;
-    top: 0;
-    left: 0;
-    background-color: black;
-    z-index: 1000;
-  `;
-  document.querySelector('html').appendChild(curtain);
-
   window.onload = () => {
     let mainContent = document.querySelector('main').innerHTML;
 
-    document.querySelector('head').innerHTML = pageHeadFrame;
     document.querySelector('body').innerHTML = pageBodyFrame;
     document.querySelector('main').innerHTML = mainContent;
 
@@ -45,24 +31,17 @@
   }
 
   const removeCurtain = () => {
-    document.querySelector('html').removeChild(curtain);
+    document.querySelector('body').classList.add('loaded');
   } 
-
-  const pageHeadFrame = `
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>RPG</title>
-    <link rel="stylesheet" href="css/style.css">
-  `;
 
   const pageBodyFrame = `
     <header class="page-header">
       <div class="top-bar">
         <h1 class="page-logo" id="page-logo">RPG</h1>
         <nav class="main-nav">
-          <a href="">Wstęp</a>
-          <a href="">Systemy&nbsp;&nbsp;RPG</a>
-          <a href="">Galeria</a>
+          <a href="index.html">Wstęp</a>
+          <a href="systems.html">Systemy&nbsp;&nbsp;RPG</a>
+          <a href="gallery.html">Galeria</a>
         </nav>
       </div>
       <div class="sword" id="sword"></div>
